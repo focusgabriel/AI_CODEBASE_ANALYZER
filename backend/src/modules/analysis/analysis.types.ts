2e5b35d5ed1854c85b37546bd5714bc3e538dcac
focusgabriel <226489347+focusgabriel@users.noreply.github.com>
2026-08-03 12:38:14 +0100

    the constants that we would use in status and sourceType not random values

---
+++ backend/src/modules/analysis/analysis.types.ts
@@ -0,0 +1,13 @@
+export enum AnalysisStatus {
+  PENDING = "PENDING",
+  UPLOADING = "UPLOADING",
+  EXTRACTING = "EXTRACTING",
+  ANALYZING = "ANALYZING",
+  COMPLETED = "COMPLETED",
+  FAILED = "FAILED",
+}
+
+export enum SourceType {
+  ZIP = "ZIP",
+  GITHUB = "GITHUB"
+}
