1ff811d746c651ae6c7dd8d4e1b4ca1649573c8e
focusgabriel <226489347+focusgabriel@users.noreply.github.com>
2026-08-03 04:05:20 +0100

    we connected the db here separately and called it in the main ts file

---
+++ backend/src/core/database/database.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { env } from "../config/env.js";
+import { logger } from "../logger/logger.js";
+
+export const connectDatabase = async () => {
+  try {
+    await mongoose.connect(env.MONGODB_URI);
+    console.log(env.MONGODB_URI);
+    logger.info("MongoDB connected.");
+  } catch (error) {
+    logger.fatal(error, "MongoDB connection failed.");
+    process.exit(1);
+  }
+};
