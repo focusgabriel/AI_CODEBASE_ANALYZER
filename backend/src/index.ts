8e6802c52af32f5d8875580f5576911afbb4417f
focusgabriel <226489347+focusgabriel@users.noreply.github.com>
2026-08-03 04:09:45 +0100

    establishing the connection here for modularity and some new concept chatgpt implemented, since we are doing things a new way in this project

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,23 @@
-import express from "express";
+import app from "./app.js";
+import { env } from "./core/config/env.js";
+import { connectDatabase } from "./core/database/database.js";
+import { logger } from "./core/logger/logger.js";
 
-const app = express();
 
-const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+await connectDatabase();
+const server = app.listen(env.PORT, () => {
+  logger.info(`Server running on http://localhost:${env.PORT}`);
 });
+
+const shutdown = (signal: string) => {
+  logger.info(`${signal} received. Shutting down...`);
+
+  server.close(() => {
+    logger.info("Server stopped.");
+    process.exit(0);
+  });
+};
+
+process.on("SIGINT", () => shutdown("SIGINT"));
+process.on("SIGTERM", () => shutdown("SIGTERM"));
