eb8d3bf361920f67a9f67b0c5b9e2677db8238fe
focusgabriel <226489347+focusgabriel@users.noreply.github.com>
2026-08-06 16:55:46 +0100

    the database metrics schema

---
+++ backend/src/models/metrics.model.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+export interface MetricsDocument {
+  analysisId: string;
+  fileId: string;
+
+  imports: number
+  exports: number
+  functions: number
+  classes: number
+  interfaces: number
+}
+
+const metricsSchema = new mongoose.Schema({
+  analysisId: {
+    type: String,
+    required: true,
+    ref: "Analysis",
+  },
+
+  fileId: {
+    type: String,
+    required: true,
+    ref: "Files",
+  },
+
+  imports: Number,
+  exports: Number,
+  functions: Number,
+  classes: Number,
+  interfaces: Number,
+
+},
+  {
+    timestamps: true,
+  }
+)
+
+export const MetricsModel = mongoose.model(
+  "Metrics", metricsSchema
+);
