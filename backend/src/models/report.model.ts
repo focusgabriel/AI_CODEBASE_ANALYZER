fa0af7b9a0136b9b79401b815cfe14c1ab2e482d
focusgabriel <226489347+focusgabriel@users.noreply.github.com>
2026-08-06 16:59:23 +0100

    the report database model

---
+++ backend/src/models/report.model.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+export interface ReportDocument {
+  analysisId: string;
+  content: string;
+}
+
+const reportSchema = new mongoose.Schema(
+  {
+    analysisId: {
+        type: String,
+        required: true,
+        unique: true,
+        ref: "Analysis",
+    },
+
+    content: {
+        type: String,
+        required: true,
+    },
+  },
+  {
+      timestamps: true,
+  },
+);
+
+export const ReportModel = mongoose.model(
+  "Report",
+  reportSchema,
+);
