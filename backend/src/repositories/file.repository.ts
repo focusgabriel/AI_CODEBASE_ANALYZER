3fe80cfba7e2bf1420458a5536d0a44d4007b2cd
focusgabriel <226489347+focusgabriel@users.noreply.github.com>
2026-08-06 16:55:04 +0100

    the code that taks to the database for file insertion and reading

--- backend/src/repositories/file.repository.ts
+++ backend/src/repositories/file.repository.ts
@@ -4,3 +4,9 @@ import { FileModel } from "../models/file.model.js";
 export async function createFiles(files: CreateFileDto[]) {
   return FileModel.insertMany(files);
 }
+
+export async function getFilesByAnalysisId(
+  analysisId: string,
+) {
+  return FileModel.find({ analysisId });
+}
