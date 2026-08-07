03be37ced35e9f457930146ff7fbb80f5e016dfe
focusgabriel <226489347+focusgabriel@users.noreply.github.com>
2026-08-07 20:40:03 +0100

    implemented the code to get the analysis baed on the authenticated user and the analysisId

--- backend/src/controllers/analysis.controller.ts
+++ backend/src/controllers/analysis.controller.ts
@@ -22,7 +22,8 @@
 // }
 
 import { Request, Response, NextFunction } from "express";
-import { createAnalysis } from "../services/analysis.services.js";
+import { createAnalysis, getAnalysisForUser } from "../services/analysis.services.js";
+import { AppError } from "../core/errors/AppError.js";
 
 export const createAnalysisController = async (
   req: Request,
@@ -45,3 +46,44 @@ export const createAnalysisController = async (
     next(error);
   }
 };
+
+
+export const getUserAnalysis = async (
+  req:Request,
+  res:Response,
+  next:NextFunction
+) => {
+  try {
+    const {analysisId, userId} = req.params
+
+    if(!analysisId || !userId) {
+      throw new AppError("Analysis ID and user ID are required", 400);
+    }
+
+    const analysis = await getAnalysisForUser(analysisId as string, userId as string);
+
+    if (!analysis) {
+      return next(
+        new AppError("Analysis not found", 404),
+      );
+    }
+
+    return res.status(200).json({
+      success:true,
+      data: {
+        name: analysis.name,
+        status: analysis.status,
+        sourceType: analysis.sourceType,
+        sourceLocation: analysis.sourceLocation,
+        reportId: analysis.reportId,
+        startedAt: analysis.startedAt,
+        completedAt: analysis.completedAt,
+        createdAt: analysis.createdAt,
+        updatedAt: analysis.updatedAt,
+      }
+
+    })
+  } catch (error) {
+    next(error);
+  }
+}
