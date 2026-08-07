c5a3d763c519e390cf1ce975cb1b662cf1c34cd4
focusgabriel <226489347+focusgabriel@users.noreply.github.com>
2026-08-07 20:33:28 +0100

    i implemented the repo to get the user for finding the analysis

--- backend/src/repositories/analysis.repository.ts
+++ backend/src/repositories/analysis.repository.ts
@@ -15,3 +15,13 @@ import { AnalysisModel } from "../models/analysis.models.js";
 export async function createAnalysisRecord(data: CreateAnalysisDto) {
   return AnalysisModel.create(data);
 }
+
+export async function findAnalysisForUser(
+  analysisId: string,
+  userId: string,
+) {
+  return AnalysisModel.findOne({
+    _id: analysisId,
+    userId,
+  })
+}
