2a8718b6dd00a1ce9aa23eb256094b5ad246f55f
focus gabriel <226489347+focusgabriel@users.noreply.github.com>
2026-08-09 00:28:04 +0100

    the repository that talks to the database for the analysisId previous file delete

--- backend/src/repositories/file.repository.ts
+++ backend/src/repositories/file.repository.ts
@@ -8,5 +8,33 @@ export async function createFiles(files: CreateFileDto[]) {
 export async function getFilesByAnalysisId(
   analysisId: string,
 ) {
-  return FileModel.find({ analysisId });
+  console.log("🔎 GET FILES FOR ANALYSIS:", analysisId);
+  const files = await FileModel.find({ analysisId });
+  console.log("🔎 FILE RECORDS FOUND:", files.length);
+
+  return files;
 }
+
+
+export async function deleteFilesByAnalysisId(
+  analysisId: string,
+) {
+  return FileModel.deleteMany({ analysisId });
+}
+
+
+
+
+// export async function getFilesByAnalysisId(
+//   analysisId: string,
+// ) {
+//   console.log("🔎 GET FILES FOR ANALYSIS:", analysisId);
+
+//   const files = await FileModel.find({
+//     analysisId,
+//   });
+
+//   console.log("🔎 FILE RECORDS FOUND:", files.length);
+
+//   return files;
+// }
