2d3f96cc35e5865a5020c6627d7c2182bcd1a297
focusgabriel <226489347+focusgabriel@users.noreply.github.com>
2026-08-12 23:51:02 +0100

    feat: add user and login DTOs for authentication

---
+++ backend/src/dtos/auth.dto.ts
@@ -0,0 +1,21 @@
+export interface UserDto{
+  name: string,
+  email: string,
+  password: string,
+  verificationToken: string,
+  verificationTokenExpires: Date
+}
+
+export interface UserDtoRespone {
+  userId: string,
+  name: string,
+  email: string,
+  // password: string,
+  // verificationToken: string,
+  // verificationTokenExpires: Date,
+}
+
+export interface LoginDtoResponse {
+  email: string,
+  password: string
+}
