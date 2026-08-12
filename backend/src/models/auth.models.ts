fa9a5e0f4cdda06cb65e8149fc279ea3e1fb78b2
focusgabriel <226489347+focusgabriel@users.noreply.github.com>
2026-08-12 23:50:29 +0100

    feat: create user model with schema and interface for authentication

---
+++ backend/src/models/auth.models.ts
@@ -0,0 +1,78 @@
+import mongoose, {Schema} from "mongoose";
+
+export interface Iuser{
+  name:string,
+  email:string,
+  password:string,
+  refreshToken?:string | null,
+  createdAt?:Date,
+  updatedAt?:Date,
+  isVerified: boolean,
+  verificationToken: string | undefined,
+  verificationTokenExpires: Date | undefined,
+  passwordResetToken?: string | undefined;
+  passwordResetExpires?: Date | undefined;
+  
+}
+
+export const UserModel = new Schema<Iuser>({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    trim: true,
+    minLength:3,
+    maxLength:50,
+  },
+
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    trim: true,
+    unique: true,
+    lowercase: true,
+    match: [/\S+@\S+\.\S+/, "Please enter a valid email address"]
+  },
+
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    trim: true
+  },
+
+  refreshToken: {
+    type: String,
+    default: null
+  },
+
+  isVerified: {
+    type: Boolean,
+    default: false
+  },
+
+  verificationToken: {
+    type: String, 
+    default: null
+  },
+
+  verificationTokenExpires: {
+    type: Date,
+    default: null
+  },
+
+  passwordResetToken: {
+    type: String,
+    default: null,
+  },
+
+  passwordResetExpires: {
+    type: Date,
+    default: null,
+  },
+
+},
+  {
+    timestamps: true
+  }
+)
+
+export const authModel = mongoose.model("Users", UserModel);
