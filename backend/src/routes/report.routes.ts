8f74ae89e52460b937c6c42441106bb86b16693c
focusgabriel <226489347+focusgabriel@users.noreply.github.com>
2026-08-13 19:56:42 +0100

    feat: add route to retrieve all reports for authenticated users

--- backend/src/routes/report.routes.ts
+++ backend/src/routes/report.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getReportController } from "../controllers/report.controller.js";
+import { getAllReportsController, getReportController } from "../controllers/report.controller.js";
 // import { errorHandler } from "../core/middlewares/errorHandler.js";
 import { authMiddleware } from "../core/middlewares/authMiddleware.js";
 
 const router = Router();
 
 router.get("/:analysisId/", authMiddleware, getReportController);
+router.get("/", authMiddleware, getAllReportsController);
 
 export default router;
