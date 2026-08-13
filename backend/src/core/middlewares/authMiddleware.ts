d1939bc4b3769fc04ff75263c6c5e404a0f264b3
focusgabriel <226489347+focusgabriel@users.noreply.github.com>
2026-08-13 14:47:06 +0100

    refactor: streamline error handling and improve code readability in authMiddleware

--- backend/src/core/middlewares/authMiddleware.ts
+++ backend/src/core/middlewares/authMiddleware.ts
@@ -1,31 +1,27 @@
-import {Request, Response, NextFunction} from "express";
+import {NextFunction, Request, Response} from "express";
 import jwt from "jsonwebtoken";
 import { AppError } from "../errors/AppError.js";
 
 export const authMiddleware = (req:Request, res:Response, next:NextFunction) => {
-
-
   const token = req.cookies.accessToken;
-  console.log("🍪 COOKIES:", req.cookies);
-console.log("📨 COOKIE HEADER:", req.headers.cookie);
 
   if (!token) {
-    throw new AppError("Unauthorized", 401);
+    return next(new AppError("Unauthorized", 401));
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET!);
-      if(typeof decoded === "string" || !("sub" in decoded)){
-        throw new AppError("Invalid Token", 401);
-      }
-      req.user = {
-        id: decoded.sub as string,
-      }
-    next()
-    } catch (error) {
-      throw new AppError("Invalid Token", 401);
+    if (typeof decoded === "string" || !("sub" in decoded)) {
+      return next(new AppError("Invalid Token", 401));
+    }
+    req.user = {
+      id: decoded.sub as string,
+    };
+    next();
+  } catch {
+    return next(new AppError("Invalid Token", 401));
   }
-}
+};
 
 
 
