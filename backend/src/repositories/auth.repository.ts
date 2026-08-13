4171067dc8585708e0effd9fe0174ca9c1f34205
focusgabriel <226489347+focusgabriel@users.noreply.github.com>
2026-08-13 03:55:06 +0100

    feat: implement authentication repository with account registration and token management functions

---
+++ backend/src/repositories/auth.repository.ts
@@ -0,0 +1,19 @@
+import { UserDto } from "../dtos/auth.dto.js";
+import { authModel } from "../models/auth.models.js";
+
+export async function RegisterAccount(data: UserDto){
+  return authModel.create(data)
+}
+
+export async function LoginAccont(email:string) {
+  return authModel.findOne({
+    email
+  })
+}
+
+export async function RefreshTokenCreation(userId: string, refreshToken: string){
+  return authModel.findOne({
+    _id: userId,
+    refreshToken
+  })
+}
