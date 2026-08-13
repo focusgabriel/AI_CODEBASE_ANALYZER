786e7e75f640a7b1e3025066d1c47c388bcfc83d
focusgabriel <226489347+focusgabriel@users.noreply.github.com>
2026-08-13 03:55:49 +0100

    feat: implement authentication service with user registration, login, and token refresh

---
+++ backend/src/services/auth.services.ts
@@ -0,0 +1,55 @@
+import { AppError } from "../core/errors/AppError.js";
+import { UserDto, UserDtoRespone } from "../dtos/auth.dto.js";
+import { LoginAccont, RefreshTokenCreation, RegisterAccount } from "../repositories/auth.repository.js";
+import bcrypt from "bcrypt";
+
+interface CreateUser {
+  name: string,
+  email: string,
+  password: string,
+  // verificationToken: string,
+  // verificationTokenExpires: Date
+}
+
+interface LoginUser{
+  email:string
+}
+
+export async function RegisterNewAccount(
+  request: CreateUser
+):Promise<UserDtoRespone>{
+  const register = await RegisterAccount(request);
+
+  return {
+    userId: register._id.toString(),
+    name: register.name,
+    email: register.email,
+  }
+}
+
+
+export async function LoginToAccount(
+  email: string
+) {
+
+  const user = await LoginAccont(email);
+
+  if(!user) {
+    throw new AppError("Invalid Credentials", 401);
+  }
+  
+  return user;
+}
+
+export async function RefreshTokenCreate(
+  userId: string,
+  refreshToken: string
+) {
+  const user = await RefreshTokenCreation(userId, refreshToken);
+
+  if (!user) {
+    throw new AppError("User not found", 401);
+  }
+
+  return user;
+}
