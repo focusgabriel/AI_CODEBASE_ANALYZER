4a484fc5218de46bc0bcf79fe7175ca0e7e963c8
focusgabriel <226489347+focusgabriel@users.noreply.github.com>
2026-08-13 03:56:22 +0100

    refactor: comment out unused sendVerificationEmail function and related imports

--- backend/src/utils/emailSender/verificationEmail.ts
+++ backend/src/utils/emailSender/verificationEmail.ts
@@ -1,19 +1,20 @@
-import apiInstance from "../../config/brevo.js";
-import verificationTemplate from "./templates/verificationTemplate.js";
+
+// import apiInstance from "../../config/brevo.js";
+// import verificationTemplate from "./templates/verificationTemplate.js";
 
 
-export const sendVerificationEmail = async (
-  email: string,
-  verificationLink: string,
-  newUser:string
-) => {
-  await apiInstance.sendTransacEmail({
-    sender: {
-      name: "Trackiu",
-      email: "charlesuchendu750@gmail.com",
-    },
-    to: [{ email }],
-    subject: "Verify your Trackio account",
-    htmlContent: verificationTemplate(verificationLink, newUser),
-  });
-};
+// export const sendVerificationEmail = async (
+//   email: string,
+//   verificationLink: string,
+//   newUser:string
+// ) => {
+//   await apiInstance.sendTransacEmail({
+//     sender: {
+//       name: "Trackiu",
+//       email: "charlesuchendu750@gmail.com",
+//     },
+//     to: [{ email }],
+//     subject: "Verify your Trackio account",
+//     htmlContent: verificationTemplate(verificationLink, newUser),
+//   });
+// };
