aa47ecfc6ef7a71de867ee65aa11f13f94d9c276
focusgabriel <226489347+focusgabriel@users.noreply.github.com>
2026-08-13 14:47:21 +0100

    feat: update auth routes to include logout and current user retrieval functionality

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
-import { LoginAccountController, RegisterAccountController } from "../controllers/auth.controller.js";
-import { errorHandler } from "../core/middlewares/errorHandler.js";
+import { getCurrentUserController, LoginAccountController, LogoutAccountController, RefreshTokenController, RegisterAccountController } from "../controllers/auth.controller.js";
 import { validate } from "../core/middlewares/validate.middleware.js";
 import { loginSchema, registerSchema } from "../validation/auth.schema.js";
-import { uploadRepositoryController } from "../controllers/upload.controller.js";
+import { authMiddleware } from "../core/middlewares/authMiddleware.js";
 
 const router = Router();
 
 router.post("/auth/register", validate(registerSchema), RegisterAccountController);
 router.post("/auth/login", validate(loginSchema), LoginAccountController);
-router.post("/refresh", uploadRepositoryController);
+router.post("/auth/logout", LogoutAccountController);
+router.post("/refresh", RefreshTokenController);
+router.get("/auth/me", authMiddleware, getCurrentUserController)
 
 export default router;
