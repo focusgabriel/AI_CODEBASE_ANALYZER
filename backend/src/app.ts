0dfcfd358a8f41a60891bdb748ee9ab720001567
focusgabriel <226489347+focusgabriel@users.noreply.github.com>
2026-08-13 03:53:47 +0100

    feat: add cookie-parser and auth routes to app configuration

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,8 +5,11 @@ import { env } from "./core/config/env.js";
 import { errorHandler } from "./core/middlewares/errorHandler.js";
 import analysisRoutes from "./routes/analysis.routes.js";
 import reportRoutes from "./routes/report.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+app.use(express.urlencoded({ extended: false }))
 
 app.use(
   cors({
@@ -17,9 +20,11 @@ app.use(
 
 app.use(express.json());
 
+app.use(cookieParser());
 app.use(errorHandler);
 app.use("/api/v1/analyses", analysisRoutes);
 app.use("/api/v1/reports", reportRoutes);
+app.use("/api/v1", authRoutes);
 // app.use("/api/v1/analyses/:upload", uploadRoutes)
 
 app.get("/health", (_, res) => {
