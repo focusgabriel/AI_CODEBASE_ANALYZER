2336533e724d640e30d4f2b5e8f05f13d462c4a3
focusgabriel <226489347+focusgabriel@users.noreply.github.com>
2026-08-15 21:14:16 +0100

    feat: add ReportField component to display analysis information

---
+++ frontend/src/components/ReportField.tsx
@@ -0,0 +1,11 @@
+import { AnalysisCard } from "./AnalysisCard"
+
+const ReportField = () => {
+  return (
+    <div>
+      <AnalysisCard icon="/images/c++.png" alt="C++" title="Hardcoding Env Variables in your code" content="use .env for sensitive information"  />
+    </div>
+  )
+}
+
+export default ReportField
