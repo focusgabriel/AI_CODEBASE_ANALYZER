530a8ab3e530e324b8dd09a5f352657d1add16d7
focusgabriel <226489347+focusgabriel@users.noreply.github.com>
2026-08-15 23:42:27 +0100

    feat: add getScoreTrend function to fetch score trend data from API

---
+++ frontend/src/services/analysisScores.services.ts
@@ -0,0 +1,15 @@
+
+import api from "../api/fetch";
+// import type { DashboardResponse } from "../types/dashboard";
+
+
+export async function getScoreTrend() {
+
+  // const [dashboardData, setDashboardData] = useState<DashboardResponse | null>(null);
+
+  const response = await api.get("/dashboard");
+
+  const trend = response.data.scoreTrend.trend
+
+  return trend;
+}
