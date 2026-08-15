d147551d0d964bc50c85fb05157fa5b431e98f38
focusgabriel <226489347+focusgabriel@users.noreply.github.com>
2026-08-15 23:41:48 +0100

    feat: remove unnecessary fields from ReportField component

--- frontend/src/components/ReportField.tsx
+++ frontend/src/components/ReportField.tsx
@@ -6,9 +6,6 @@ const ReportField = () => {
       <div className="flex justify-between items-center borderBottom *:font-semibold *:text-[#848484] *:text-[14px]">
         <p>Top Issues</p>
         <p>Recommendations</p>
-        <p>Architecture</p>
-        <p>Security</p>
-        <p>Risks</p>
         <p>Overview</p>
       </div>
     
