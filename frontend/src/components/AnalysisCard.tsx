e72fe3aabcdd41898d1accd8f0ba7e5c6f662055
focusgabriel <226489347+focusgabriel@users.noreply.github.com>
2026-08-15 18:21:55 +0100

    feat: add AnalysisCard component for displaying analysis results

---
+++ frontend/src/components/AnalysisCard.tsx
@@ -0,0 +1,28 @@
+import type { JSX } from "react/jsx-runtime";
+
+interface AnalysisProps {
+  icon: string,
+  alt: string,
+  title: string,
+  content: string,
+  score?: JSX.Element
+}
+
+export const AnalysisCard = ({icon, alt, title, content, score}: AnalysisProps) => {
+  return(
+    <div className="flex justify-between bg-white border border-gray-500 rounded-xl h-auto w-[260px] mx-auto px-2">
+      <div className="items-center p-2">
+        <img src={icon} alt={alt} width={30} height={30} className="object-contain" />
+      </div>
+
+      <div className="flex flex-col items-center ml-[-4em]">
+        <h2 className="text-[14px]">{title}</h2>
+        <p className="text-[12px]">{content}</p>
+      </div>
+
+      <div className=" flex justify-end items-center">
+        <p>{score}</p>
+      </div>
+    </div>
+  )
+}
