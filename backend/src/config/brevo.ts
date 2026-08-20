33f082e1572dd4cc43bfaf9d3e7b3e528cfb2dd8
focusgabriel <focusgabriel@users.noreply.github.com>
2026-08-20 23:35:09 +0100

    feat: enable Brevo client configuration by uncommenting relevant code

--- backend/src/config/brevo.ts
+++ backend/src/config/brevo.ts
@@ -1,23 +1,23 @@
-// import { BrevoClient, Brevo } from "@getbrevo/brevo";
+import { BrevoClient, Brevo } from "@getbrevo/brevo";
 
-// console.log(process.env.BREVO_API_KEY)
-// const apiKey = process.env.BREVO_API_KEY;
+console.log(process.env.BREVO_API_KEY)
+const apiKey = process.env.BREVO_API_KEY;
 
-// if (!apiKey) {
-//   throw new Error("Missing BREVO_API_KEY environment variable");
-// }
+if (!apiKey) {
+  throw new Error("Missing BREVO_API_KEY environment variable");
+}
 
-// const brevoClient = new BrevoClient({
-//   apiKey,
-// });
+const brevoClient = new BrevoClient({
+  apiKey,
+});
 
-// const apiInstance: {
-//   sendTransacEmail: (
-//     request: Brevo.SendTransacEmailRequest
-//   ) => Promise<Brevo.SendTransacEmailResponse>;
-// } = {
-//   sendTransacEmail: (request) =>
-//     brevoClient.transactionalEmails.sendTransacEmail(request),
-// };
+const apiInstance: {
+  sendTransacEmail: (
+    request: Brevo.SendTransacEmailRequest
+  ) => Promise<Brevo.SendTransacEmailResponse>;
+} = {
+  sendTransacEmail: (request) =>
+    brevoClient.transactionalEmails.sendTransacEmail(request),
+};
 
-// export default apiInstance;
+export default apiInstance;
