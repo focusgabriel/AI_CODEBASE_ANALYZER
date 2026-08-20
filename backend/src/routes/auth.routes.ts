38b98c6220b565544441bb9fcb6a8daae9fba7fb
focusgabriel <focusgabriel@users.noreply.github.com>
2026-08-20 23:40:50 +0100

    feat: add routes for email verification and password reset functionality

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getCurrentUserController, LoginAccountController, LogoutAccountController, RefreshTokenController, RegisterAccountController } from "../controllers/auth.controller.js";
+import { forgotPasswordController, getCurrentUserController, LoginAccountController, LogoutAccountController, RefreshTokenController, RegisterAccountController, ResetPasswordController, verificationAccountController } from "../controllers/auth.controller.js";
 import { validate } from "../core/middlewares/validate.middleware.js";
 import { loginSchema, registerSchema } from "../validation/auth.schema.js";
 import { authMiddleware } from "../core/middlewares/authMiddleware.js";
@@ -12,5 +12,8 @@ router.post("/auth/login", authLimiter, validate(loginSchema), LoginAccountContr
 router.post("/auth/logout", LogoutAccountController);
 router.post("/refresh", RefreshTokenController);
 router.get("/auth/me", authMiddleware, getCurrentUserController)
+router.get("/auth/verify-email/:token", verificationAccountController);
+router.post("/auth/forgot-password/", authLimiter, forgotPasswordController);
+router.post("/auth/reset-password/:token", authLimiter, ResetPasswordController);
 
 export default router;
