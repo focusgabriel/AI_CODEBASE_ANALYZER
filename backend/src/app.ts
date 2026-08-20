33930c9b31323796a15350b070695561c96da5d8
focusgabriel <focusgabriel@users.noreply.github.com>
2026-08-20 19:57:20 +0100

    feat: add request rate limiting to analysis, report, metrics, upload, dashboard, and file routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,8 +11,16 @@ import fileRoutes from "./routes/file.routes.js";
 import uploadRoutes from "./routes/upload.routes.js";
 import cookieParser from "cookie-parser";
 import dashboardRoutes from "./routes/dashboard.routes.js"
+import { limiter } from "./validation/limiter.js";
+import helmet from "helmet";
 
 const app: Application = express();
+
+
+app.set('trust proxy', 1);
+
+app.use(helmet());
+
 app.use(
   cors({
     origin: env.CLIENT_URL,
@@ -28,13 +36,13 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use(cookieParser());
 
-app.use("/api/v1/analyses", analysisRoutes);
-app.use("/api/v1/reports", reportRoutes);
+app.use("/api/v1/analyses", limiter, analysisRoutes);
+app.use("/api/v1/reports", limiter, reportRoutes);
 app.use("/api/v1", authRoutes);
-app.use("/api/v1/metrics", metricsRoutes);
-app.use("/api/v1/upload", uploadRoutes);
-app.use("/api/v1/dashboard", dashboardRoutes);
-app.use("/api/v1/files", fileRoutes);
+app.use("/api/v1/metrics", limiter, metricsRoutes);
+app.use("/api/v1/upload", limiter, uploadRoutes);
+app.use("/api/v1/dashboard", limiter, dashboardRoutes);
+app.use("/api/v1/files", limiter, fileRoutes);
 // app.use("/api/v1/analyses/:upload", uploadRoutes)
 app.use(errorHandler);
 
@@ -46,3 +54,4 @@ app.get("/health", (_, res) => {
 });
 
 export default app;
+
