131f7e0137781c42e521ffeb900372fe9ab34a1d
focusgabriel <focusgabriel@users.noreply.github.com>
2026-08-20 14:47:00 +0100

    refactor: comment out unused getAnalysisFile function and related imports

--- frontend/src/services/files.services.ts
+++ frontend/src/services/files.services.ts
@@ -1,14 +1,14 @@
-import api from "../api/fetch";
-import type { GetAnalysisFileResponse } from "../types/file-explorer";
+// import api from "../api/fetch";
+// import type { GetAnalysisFileResponse } from "../types/file-explorer";
 
-export async function getAnalysisFile(
-  analysisId: string,
-  fileId: string,
-) {
-  const response =
-    await api.get<GetAnalysisFileResponse>(
-      `files/analyses/${analysisId}/files/${fileId}`,
-    );
+// export async function getAnalysisFile(
+//   analysisId: string,
+//   fileId: string,
+// ) {
+//   const response =
+//     await api.get<GetAnalysisFileResponse>(
+//       `files/analyses/${analysisId}/files/${fileId}`,
+//     );
 
-  return response.data.data;
-}
+//   return response.data.data;
+// }
